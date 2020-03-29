97299ed9d5ae648ecdb801b1ea8f0f017d6236e8
niww <niww@ya.ru>
2020-03-29 23:31:12 +0300

    And custom view

--- app/src/main/java/com/example/trainingbody/MainActivity.kt
+++ app/src/main/java/com/example/trainingbody/MainActivity.kt
@@ -2,12 +2,20 @@ package com.example.trainingbody
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main
+        val textView = TextView(this)
+        textView.text = "Work it"
+        val ll = LinearLayout(this)
+        ll.addView(textView)
+        setContentView(ll)
     }
+
 
 }
